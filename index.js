1f212322598f7e87f08cdeaf78fa36c0b880a615
wellsousaaa <wendell25sousaferreira@gmail.com>
2021-10-02 13:53:47 -0300

    fix: dinamic port

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ async function handleMessage(client, message) {
   }
 }
 
+const port = process.env.PORT || 5000;
 const server = http.createServer(requestListener);
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log(`Server is running`);
 });
