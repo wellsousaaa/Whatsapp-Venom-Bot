e7620200b8f5a0dfa52565b2aca14ef189b72293
wellsousaaa <wendell25sousaferreira@gmail.com>
2021-10-02 03:19:49 -0300

    feat: main endpoint

---
+++ index.js
@@ -0,0 +1,38 @@
+const venom = require("venom-bot");
+
+const MusicController = require("./src/controllers/MusicController");
+const StickerController = require("./src/controllers/StickerController");
+
+venom
+  .create()
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => handleMessage(client, message));
+}
+
+async function handleMessage(client, message) {
+  const commandIs = (string, caption = false) =>
+    caption
+      ? message.caption?.indexOf(string) >= 0
+      : message.body?.indexOf(string) >= 0;
+
+  // console.log(message);
+
+  /// Get Music from URL
+  if (commandIs("--music-url")) MusicController.fromURL(client, message);
+  /// Get Music from Search
+  else if (commandIs("--music-search"))
+    MusicController.fromSearch(client, message);
+  /// Image and GIF to Sticker
+  else if (commandIs("--fig-url"))
+    StickerController.URLToSticker(client, message);
+  else if (commandIs("--fig", true))
+    StickerController.imageToSticker(client, message);
+  else {
+    await client.clearChatMessages(message.from);
+  }
+}
